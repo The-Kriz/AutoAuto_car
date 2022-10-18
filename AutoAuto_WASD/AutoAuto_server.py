84be9be48278a6853a3d0d0ca985ca45f63a30bf
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2022-10-18 11:25:47 +0530

    Add files via upload

---
+++ AutoAuto_WASD/AutoAuto_server.py
@@ -0,0 +1,30 @@
+# SENDER
+import time
+import socket
+from keyboard import is_pressed
+
+ip = "192.168.1.193"
+port = 8089
+msg = b" "
+while True :
+    try:
+        if is_pressed('w'):
+            msg = b'w'
+        elif is_pressed('a'):
+            msg = b'a'
+        elif is_pressed('s'):
+            msg = b's'
+        elif is_pressed('d'):
+            msg = b'd'
+    except:
+        break
+        
+    
+
+    print(f'Sending {msg} to {ip}:{port}')
+
+    sock = socket.socket(socket.AF_INET,
+                            socket.SOCK_DGRAM)
+    sock.sendto(msg, (ip, port))
+    time.sleep(0.2)
+    msg = b' '
